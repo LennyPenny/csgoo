08c8689f61b28012f46c49f3e2181263c2932395
LennyPenny <l.bernhardt@live.de>
2015-05-26 21:56:21 +0200

    remove unused crap

--- premake5.lua
+++ premake5.lua
@@ -8,25 +8,6 @@ solution "cross"
 	location ("project " .. _ACTION)
 	targetdir ( "build/" )
 	startproject "crossproc"
-
-	--[[includedirs {
-		"deps",
-		SOURCE_SDK .. "/common",
-		SOURCE_SDK .. "/public/tier0",
-		SOURCE_SDK .. "/public/tier1",
-		SOURCE_SDK .. "/public"
-	}
-
-	libdirs {
-		SOURCE_SDK .. "/lib/public"
-	}
-
-	links {
-		"tier0",
-		"tier1",
-		"vstdlib",
-		"Shlwapi"
-	}]]
 	
 	configurations
 	{ 
@@ -45,23 +26,11 @@ solution "cross"
 		linkoptions {
 			"/NODEFAULTLIB:libcmt"
 		}
-	files{
-		"src/shared/**.cpp",
-		"src/shared/**.h"
-	}
-
-	project "crossproc"
-		kind "ConsoleApp"
-		
-		files {
-			"src/crossproc/**.cpp",
-			"src/crossproc/**.h"
-		}
 
 	project "crossdll"
 		kind "SharedLib"
 		
 		files {
-			"src/crossdll/**.cpp",
-			"src/crossdll/**.h"
+			"src/**.cpp",
+			"src/**.h"
 		}
